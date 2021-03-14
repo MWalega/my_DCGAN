20089a3e93d40281b4c6c49ee7436aa8b1a78470
Maciek Walega <mwaleg.sa@gmail.com>
2021-03-14 23:06:27 +0100

    Added Generator

--- DCGAN.py
+++ DCGAN.py
@@ -34,3 +34,35 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.disc(x)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            # Input: N x z_dim x 64 x 64
+            self._block(z_dim, features_g*16, 4, 1, 0), # 4x4
+            self._block(features_g*16, features_g*8, 4, 2, 1), # 8x8
+            self._block(features_g*8, features_g*4, 4, 2, 1), # 16x16
+            self._block(features_g*4, features_g*2, 4, 2, 1), # 32x32
+            nn.ConvTranspose2d(
+                features_g*2, channels_img, kernel_size=4, stride=2, padding=1,
+            ),
+            nn.Tanh(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn. Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.gen(x)
